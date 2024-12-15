4bedcf8f27eb35134ac0de0c0c544a5f546c52cd
David Schurer <david.schurer@yahoo.com>
2024-12-15 05:44:00 -0500

    finished task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,10 +7,12 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+return users.some(user => user.username === username);
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+return users.some(user => user.username === username && user.password === password);
 }
 
 //only registered users can login
@@ -19,6 +21,10 @@ regd_users.post("/login", (req,res) => {
 
   if (!username || !password) {
     return res.status(400).json({ message: "Both username and password are required" });
+  }
+
+  if (!authenticatedUser(username, password)) {
+    return res.status(401).json({ message: "Invalid username or password" });
   }
 
   const token = jwt.sign({ username }, "fingerprint_customer", { expiresIn: '1h' });
@@ -54,14 +60,48 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     return res.status(404).json({ message: "Book not found" });
     }
 
+    if (!book.reviews) {
+        book.reviews = {};
+    }
+
     if (book.reviews[username]) {
         book.reviews[username] = review;
         return res.status(200).json({ message: "Review updated successfully", book });
     } else {
-        book.review[username] = review;
+        book.reviews[username] = review;
         return res.status(201).json({ message: "Review added successgully", book });
     }
   });
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const { isbn } = req.params;
+
+    const token = req.session.accessToken;
+    if (!token) {
+        return res.status(401).json({ message: "Access token is missing: Please log in." });
+    }
+
+    jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: "Invalid or expired token." });
+        }
+
+        const username = decoded.username;
+
+        const book = books[isbn];
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        if (!book.reviews || !book.reviews[username]) {
+            return res.status(404).json({ message: "Review not found for this user." });
+        }
+
+        delete book.reviews[username];
+
+        return res.status(200).json({ message: "Review deleted successfully", book });
+    });
 });
 
 module.exports.authenticated = regd_users;

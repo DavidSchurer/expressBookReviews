3415f2bb97e1aafc6ee1d35bff3814ff5858e988
DavidSchurer <david.schurer@yahoo.com>
2024-12-14 22:33:11 -0500

    finished tasks 1-6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,8 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Both username and password are required" });
+  }
+
+  if (users[username]) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  users[password] = { password };
+
+  return res.status(201).json({ message: "User registered successfully" });
 });
 
 // Get the book list available in the shop
@@ -17,26 +28,48 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+    if (book) {
+        return res.status(200).send(JSON.stringify(book, null, 2));
+    } else {
+        return res.status(404).json({ message: "Book not found" });
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author.toLowerCase();
+  const matchingBooks = Object.values(books).filter(book => book.author.toLowerCase() === author);
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).send(JSON.stringify(matchingBooks, null, 2));
+  } else {
+    return res.status(404).json({ message: "No books found for the given author "});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title.toLowerCase();
+    const matchingBooks = Object.values(books).filter(book => book.title.toLowerCase() === title);
+
+    if (matchingBooks.length > 0) {
+        return res.status(200).send(JSON.stringify(matchingBooks, null, 2));
+    } else {
+        return res.status(404).json({ message: "No books found for the given title."});
+    }
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const book = books[isbn];
+  if (book && book.reviews) {
+    return res.status(200).send(JSON.stringify(book.reviews, null, 2));
+  } else {
+    return res.status(404).json({ message: "No reviews found for the given ISBN" });
+  }
 });
 
 module.exports.general = public_users;

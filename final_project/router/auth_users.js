c36fb69809bd9ec25fadacce642034fc32cb12f9
DavidSchurer <david.schurer@yahoo.com>
2024-12-14 23:13:54 -0500

    finished task 7

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -15,14 +15,53 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    return res.status(400).json({ message: "Both username and password are required" });
+  }
+
+  const token = jwt.sign({ username }, "fingerprint_customer", { expiresIn: '1h' });
+
+  req.session.accessToken = token;
+
+  return res.status(200).json({ message: "Login successful", token });
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { isbn } = req.params;
+  const { review } = req.query;
+
+  if (!review) {
+    return res.status(400).json({ message: "Review content is required" }); 
+  }
+
+  const token = req.session.accessToken;
+  if (!token) {
+    return res.status(401).json({ message: "Access token is missing: Please log in." });
+  }
+
+  jwt.verify(token, "fingerprint_customer", (err, decoded) => {
+    if (err) {
+        return res.status(403).json({ message: "Invalid or expired token." });
+    }
+
+    const username = decoded.username;
+
+    const book = books[isbn];
+    if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+    }
+
+    if (book.reviews[username]) {
+        book.reviews[username] = review;
+        return res.status(200).json({ message: "Review updated successfully", book });
+    } else {
+        book.review[username] = review;
+        return res.status(201).json({ message: "Review added successgully", book });
+    }
+  });
 });
 
 module.exports.authenticated = regd_users;
